79a357dca8a1142289dcdb0e297e4ca647fe6c57
roy <roy.chen@cifang.hk>
2021-07-22 08:57:24 +0800

    发布开源代码

---
+++ Cli.php
@@ -0,0 +1,383 @@
+<?php
+/**
+ * User: roy
+ * Date: 2016/12/6
+ */
+
+namespace iry\cli;
+
+ class Cli
+ {
+	 private static $_cfg=['disable_style'=>false,'charset'=>'utf-8'];
+
+ 	 private static $_staticCache = [];
+
+     static private $_fgColor = [
+         'black'=>'0;30',
+         'dark_gray'=>'1;30',
+         'blue'=>'0;34',
+         'light_blue'=>'1;34',
+         'green'=>'0;32',
+         'light_green'=>'1;32',
+         'cyan'=>'0;36',
+         'light_cyan'=>'1;36',
+         'red'=>'0;31',
+         'light_red'=>'1;31',
+         'purple'=>'0;35',
+         'light_purple'=>'1;35',
+         'brown'=>'0;33',
+         'yellow'=>'1;33',
+         'light_gray'=>'0;37',
+         'white'=>'1;37',
+     ];
+     static private $_bgColor = [
+         'black' => '40',
+         'red' => '41',
+         'green' => '42',
+         'yellow' => '43',
+         'blue' => '44',
+         'magenta' => '45',
+         'cyan' => '46',
+         'light_gray' => '47',
+     ];
+
+     static function __callStatic($name, $arguments)
+     {
+         //兼容老代码
+         $_name = strtolower($name);
+         $alias = [
+             'stdselect'=>'select',
+             'stdconfirm'=>'confirm'
+         ];
+         if(isset($alias[$_name])) {
+             return call_user_func_array([__CLASS__, $alias[$_name]], $arguments);
+         }else{
+             self::stdout( __CLASS__.'::'.$name." not found;",'error');
+             exit;
+         }
+     }
+
+     static private function _getScreenSize(){
+     	if(!isset(self::$_staticCache['screen_size'])){
+			$t = trim(exec('stty size'));
+			$size = explode(' ',$t);
+			self::$_staticCache['screen_size'] = ['w'=>$size[1],'h'=>$size[0]];
+		}
+		 return self::$_staticCache['screen_size'];
+	 }
+
+	 /**
+	  * 禁用样式
+	  * @param bool $status
+	  */
+	 static public function disableStyle($status){
+         self::$_cfg['disable_style'] = (bool)$status;
+	 }
+
+     /**
+      * @param $charset 'utf-8', 'gbk', 'gb2312'
+      */
+	 static public function charset($charset){
+         self::$_cfg['charset'] = $charset;
+     }
+
+     // Returns colored string
+     static public function getColoredString($string, $foreground_color = null, $background_color = null)
+     {
+         if(self::$_cfg['disable_style']){
+             return $string;
+         }else {
+             $colored_string = "";
+             // Check if given foreground color found
+             if ($foreground_color && isset(self::$_fgColor[$foreground_color])) {
+                 $colored_string .= "\033[" . self::$_fgColor[$foreground_color] . "m";
+             }
+             // Check if given background color found
+             if ($background_color && isset(self::$_bgColor[$background_color])) {
+                 $colored_string .= "\033[" . self::$_bgColor[$background_color] . "m";
+             }
+             // Add string and end coloring
+             $colored_string .= $string . "\033[0m";
+             return $colored_string;
+         }
+     }
+
+     /**
+      * 选择对话
+      * @param $arr
+      * @param int $colQty
+      * @param string $msg
+      * @param bool $mul 多选
+      * @return mixed|string|void
+      */
+
+     static function select($arr,$colQty=1,$msg='请选择',$mul=false){
+         $n = 1;
+         $row = 0;
+         if(empty($arr)){echo ("选项列表有误");return ;}
+         self::stdout("\n---------------------------\n");
+         foreach ($arr as $k=>$v){
+             //$color = ($row%2)===0? 'while' :'light_gray';
+             $color = 'while';
+             self::stdout(" $k. ",'highlight');
+             self::stdout($v, [$color, null]);
+             if($n<$colQty){
+                 $n++;
+                 self::stdout( "\t\t");
+             }else{
+                 $n=1;
+                 $row++;
+                 self::stdout( "\n- - - - - - - - - - - - - - -",['light_gray',null]);
+                 self::stdout( "\n");
+             }
+         }
+         self::stdout( "\n---------------------------\n");
+
+
+         $r = [];
+         $msg = trim($msg)===''?'请选择':$msg;
+         while (true){
+             $tmpRes = self::stdin($msg.":\t",function($v) use ($arr,$mul){
+                 return (is_numeric($v) && isset($arr[$v])) || ($mul && in_array($v,['#','<','*']));
+             },function ($v){return trim($v);});
+
+             if(!$mul){
+                 return $tmpRes;
+             }else {
+				 if ($tmpRes === '#') {
+					 break;
+				 } elseif ($tmpRes === '<') {
+					 array_pop($r);
+				 } elseif ($tmpRes === '*') {
+					 $r = [];
+				 } else {
+					 $r[] = $tmpRes;
+					 $r = array_unique($r);
+				 }
+				 echo "\033[1A\033[K";
+				 self::stdout('当前已选择:[' . implode(',', $r) . ']', 'comment');
+				 $msg = "请继续选择[#结束,<回退,*清空]";
+			 }
+         }
+         return $r;
+     }
+
+     /**
+      * 确认对话
+      * @param string $msg
+      * @return bool
+      */
+
+     static function confirm($msg){
+         $r = self::stdin($msg." [y/n] ",function($v){return ($v==='y'||$v==='n');},function ($v){return strtolower(trim($v));});
+         return $r==='y'? true: false;
+     }
+
+     /**
+      * 标准输入
+      * @param string $msg
+      * @param bool $validator
+      * @param bool $processor
+      * @param int $limitLen
+      * @return string
+      */
+
+     static function stdin($msg='',$validator=false,$processor = false,$limitLen=100000){
+         if($msg) {
+             if (strpos($msg, '[span]')) {
+                 self::output($msg . ':');
+             } else {
+                 self::stdout($msg . ':');
+             }
+         }
+         $stdin=fopen('php://stdin','r');
+         $content=trim(fgets($stdin,$limitLen));
+         fclose($stdin);
+
+         if(is_callable($processor)){
+             $content = call_user_func($processor,$content);
+         }
+         if(is_callable($validator) && !call_user_func($validator,$content) ){
+             //if($callback && !$callback($content)){
+             self::stdout("[error]",'highlight');
+             self::stdout('输入不正确!请重新输入.','comment');
+             self::stdout("\n");
+             $content = self::stdin($msg, $validator,$processor,$limitLen);
+         }
+         return $content;
+     }
+
+     /**
+      * 标准输出
+      * @param $str
+      * @param bool $type error,info,comment,question,highlight,warning
+      * @param bool $return
+      * @return string
+      */
+
+     static public function stdout($str,$type = false,$return = false){
+
+         if(is_array($str)){
+             $r = '';
+             foreach ($str as $item){
+                 if(is_array($item)){
+                     $style = isset($item['style'])?$item['style']:(isset($item[1])?$item[1]:$type);
+                     $text = (string)( isset($item['text'])?$item['text']:(isset($item[0])?$item[0]:'') );
+                     $r .= self::stdout($text, $style, $return);
+                 }else{
+					 $r .= self::stdout((string)$item, $type, $return);
+                 }
+             }
+			 if ($return) {
+				 return $r;
+			 } else {
+				 echo $r;
+			 }
+         }else {
+             $typeList = [
+                 'error' => ['white', 'red'],
+                 'info' => ['green', null],
+                 'comment' => ['yellow', null],
+                 'question' => ['black', 'cyan'],
+                 'highlight' => ['red', null],
+                 'warning' => ['black', 'yellow'],
+             ];
+             if (is_array($type)) {
+                 $color = $type;
+                 $color[0] = isset($color[0]) ? $color[0] : null;
+                 $color[1] = isset($color[1]) ? $color[1] : null;
+             } else {
+                 $type = $type ? trim($type, '[ ]') : $type;
+                 $color = ($type && isset($typeList[$type])) ? $typeList[$type] : false;
+             }
+             if (PHP_OS === 'WINNT') {
+                 $str = str_replace("\r\n", "\n", $str);
+                 $str = str_replace("\n", "\r\n", $str);
+                 //$str = iconv('utf-8', 'gbk', $str);
+             }
+             if (self::$_cfg['charset'] != 'utf-8') {
+                 $str = iconv('utf-8', self::$_cfg['charset'], $str);
+             }
+
+             if ($return) {
+                 return $type ? self::getColoredString($str, $color[0], $color[1]) : $str;
+             } else {
+                 echo $type ? self::getColoredString($str, $color[0], $color[1]) : $str;
+                 return '';
+             }
+         }
+		 return '';
+     }
+
+	 /**
+	  * 混合输出
+	  * @param string $str error:[white, red], info:[green, null],comment:[yellow, null],
+	  *             question:[black, cyan],highlight:[red, null],warning:[black, yellow],
+	  */
+
+     static public function output($str)
+     {
+         //$str = is_array($str)? implode("\n", $str): $str;
+         //echo iconv('utf-8','gbk',$str);
+        $arr = explode("\n",$str);
+
+         $rowQty = count($arr);
+         $i = 0;
+         foreach($arr as $row) {
+             $i++;
+             $rowList = explode('[span]',$row);
+             foreach($rowList as $v) {
+                 $type = false;
+                 if(isset($v[0]) && $v[0]==='[' && strpos($v,']')>2  ){
+                     $tmp = explode(']',$v);
+                     $type = $tmp[0];unset($tmp[0]);
+                     //$type = trim(trim($type),'[ ]');
+                     //var_export($type);
+                     $v = implode("]",$tmp);
+                 }
+                 //var_export([$v,$type]);
+                 self::stdout($v,$type);
+             }
+             if($i<$rowQty){echo "\n";}
+         }
+     }
+
+     /**
+      * 移动光标
+      * @param string $position l:left r:ring u:up, d:down
+      * @param $n
+      */
+
+     static public function cursorMove($position,$n=1){
+         $position = strtolower($position[0]);
+         $list = ['u'=>'A','d'=>'B','l'=>'D','r'=>'C'];
+         echo '\033['.$n.$list[$position];
+     }
+
+     /**
+      * 设置关光标位置
+      * @param $x
+      * @param $y
+      */
+
+     static public function cursorPosition($x,$y){
+        echo '\033['.$y.';'.$x.'H';
+     }
+
+     /**
+      * 清屏
+      */
+     static public function clear(){
+         echo "\033[2J";
+     }
+
+     /**
+      *
+      * @param int $s 秒数
+      * @param string $msg 消息
+      */
+     static public function wait($s,$msg=''){
+         $totalLen = 20;
+		 $disabledColor = self::$_cfg['disable_style'];
+		 $colorBar = !($disabledColor || PHP_OS === 'WINNT');
+         for ($i=$s;$i>0;$i--){
+			 sleep(1);
+
+             $okLeng = floor(($s-$i+1)/$s*$totalLen);
+
+			 if($colorBar) {
+				 $str = str_pad($i.'/'.$s,$totalLen," ",STR_PAD_BOTH);
+
+				 $waitStr = substr($str,0,$totalLen-$okLeng);//str_pad(,$totalLen-$okLeng, " " );
+				 $okStr = substr($str,$totalLen-$okLeng);//str_pad(,$okLeng," ");
+				 self::stdout(["[$s]秒倒计时 [", [$waitStr, ['black', 'cyan']], [$okStr, [null, 'dark_gray']], '] ']);
+
+			 }else{
+
+				 $waitStr = str_pad('',$totalLen-$okLeng, '=' );
+				 $okStr = str_pad('', $okLeng,'-');
+				 self::stdout("[$s]秒倒计时 [$waitStr$okStr] $i/$s.");
+			 }
+
+             self::stdout($msg);
+
+             echo "\r";
+
+             //echo "\r\033[K";
+         }
+     }
+
+	 /**
+	  * @param int $total
+	  * @param int $current
+	  * @param string $msg
+	  */
+     static public function progressBar($total,$current,$msg=''){
+		//
+		 $len = 60;
+		 $c = floor($len/$total*$current);
+		 $bar = str_pad('',$c,'=').str_pad('',$len-$c,'-');
+		 self::stdout("[$bar] $current/$total $msg     \r");
+		 //self::stdout("[$s]秒倒计时 [".$okStr.$waitStr.']'.$i.'/'.$s.'.');
+	 }
+}
---
+++ README.md
@@ -0,0 +1,96 @@
+#简介
+
+ir/Cli 可以简单实现 输出、输入、倒计时、绘制进度条、绘制表格、移动光标、清除屏幕、确认对话 等功能
+
+可以轻松实现类似表单等输入功能：
+
+如（input: _stdin_, 开关:_confirm_, select:select，radio:_select单选模式_, checkbox:_select多选模式_）
+# 环境控制
+Cli::disableStyle(true|false)//是否禁用样式
+Cli::charset('')//'utf-8', 'gbk', 'gb2312'
+
+# 示例
+eg:
+## 标准输入 
+stdin($msg,$validator,$processor);//等待并获取用户输入的数据
+
+|  参数    |类型| 说明  |
+|  ----    |----| ----  |
+|$msg      |string|消息：等待用户输入前，输一条提示消息消息 |
+|$validator|false/callable|验证器:用于验证用户输入的信息是否有效的方法|
+|$processor|false/callable|处理器:对用户输入的数据进行处理的方法|
+
+```php
+use ir\cli\Cli;
+$numeric = Cli::stdin('请输入');//等待并获取用户输入的数据
+$numeric = Cli::stdin('请输入数字',function(){return preg_match('/^[0-9]+$/');},'trim');
+$numeric = Cli::stdin('请输入数字',function(){return preg_match('/^[0-9]+$/');},'trim');
+```
+## 标准输出 stdout($msg,$styleType)
+仅仅打印到屏幕 支持彩色文字
+
+|参数    ||说明  |
+|  ----    |----| ----  |
+|$msg      |string/array|消息：等待用户输入前，输一条提示消息消息 |
+|$styleType |string/array|error,info,comment,question,highlight,warning,[前景色,背景色]|
+$msg数组模式可以设置复杂的彩色文字如下示例
+
+```php
+[
+    '这是',
+    ['一个','error'],//使用error样式
+    ['测试','info']//使用info样式
+    //string || ['$msg','$styleType']
+];
+```
+## 确认对话
+confirm($msg,$validator,$processor);//Cli中 确认对话
+
+|参数    ||说明  |
+|  ----    |----| ----  |
+|$msg      |string|消息：等待用户输入前，输一条提示消息消息 |
+|return |bool| true/false|
+
+$msg
+```php
+use ir\cli\Cli;
+$confirm = Cli::confirm('你确认要删除吗?');
+
+//效果如下
+//你确认要删除吗?[y/n] _
+```
+
+## 列表选择
+select($list,$colQty,$msg,$mul);
+
+|参数    |||说明  |
+|  ----    |----|----| ----  |
+|$list |array|_必须_|可选清单 |
+|$colQty|int|可选|列数，展示可选清单时，展示几列。默认：1|
+|$msg|string|可选|默认：请输入|
+|$mul|bool|可选|是否允许多选，默认：false|
+
+## 移动光标
+cursorMove(方位,移动量)
+方位：l:left r:ring u:up, d:down
+```php
+use ir\cli\Cli;
+$confirm = Cli::cursorMove('u',3); //光标上移三行
+$confirm = Cli::cursorMove('l',4); //光标左移4个字符
+```
+
+## 光标定位 _(移动光标到指定位置)_
+cursorPosition(x,y) y行x列
+```php
+use ir\cli\Cli;
+$confirm = Cli::cursorPosition(3,5);//光标定位到5行第三个字符后面
+``` 
+
+## clear
+clear();//清除屏幕
+
+## wait (等待/倒计时/模拟loading 等场景)
+wait(int $s 秒,string $msg 消息);
+
+## progressBar (进度条绘制)
+progressBar(int $total 总量,int $current 当前量, string $msg 文本信息)
